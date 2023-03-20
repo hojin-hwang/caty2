b5e517a99615c429eb2d6bafc1897dde81696514
jake <hoinh@gmail.com>
2023-03-20 10:07:48 +0900

    Revert "Revert "title 1""
    This reverts commit 824f8398448f81b98b6b8b6813a92fa4f5b5e708.

--- src/App.vue
+++ src/App.vue
@@ -147,6 +147,10 @@ console.log(3)
 
 <template>
   <titl>Go Hell</titl>
+
+  <section>New Box</section>
+
+  <section>Old Box</section>
   <p>{{ answer }}</p>
   <p>{{idInfo}}</p>
   <span>X : {{x}}</span> |
